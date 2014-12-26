37a7729aeddd761b3c1a95b15e675558e1e80ab1
Zhizhik <zhizhik@iMac-Zhizhik.local>
2014-12-26 09:25:11 -0800

    Initial Commit

---
+++ LandingPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LandingPage
+//
+//  Created by Zhizhik on 26.12.14.
+//  Copyright (c) 2014 c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LandingPage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LandingPage
+//
+//  Created by Zhizhik on 26.12.14.
+//  Copyright (c) 2014 c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
